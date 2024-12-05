83e1691de77335c25a239691bbd4a65cd98b824b
bocyaa <xabirovbob@gmail.com>
2024-12-05 21:13:48 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "338b18ca-c786-4159-9f8a-425a7aa69dfe";
+export const MODAL_CLOSE_SEC = 2.5;
